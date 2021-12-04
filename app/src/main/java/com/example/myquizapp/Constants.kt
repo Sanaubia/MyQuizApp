2f4e969aa4eb9f07dc57780246fef5d621c6cbe5
Janne Prokkola <jprokkola@gmail.com>
2021-12-04 10:15:53 +0200

    - Questions added - Single question activity done

---
+++ app/src/main/java/com/example/myquizapp/Constants.kt
@@ -0,0 +1,42 @@
+package com.example.myquizapp
+
+object Constants {
+
+    fun getQuestions():ArrayList<Question>
+    {
+        val questionsList = ArrayList<Question>()
+
+        val que1 = Question(
+            1,"What country does this flag belong to?",
+            R.drawable.ic_flag_of_argentina,
+            "Argentina",
+            "Germany",
+            "Finland",
+            "Russia",
+            1
+        )
+        questionsList.add(que1)
+
+        val que2 = Question(
+            2,"What country does this flag belong to?",
+            R.drawable.ic_flag_of_australia,
+            "Kongo",
+            "U.S.A",
+            "Canada",
+            "Australia",
+            2
+        )
+        val que3 = Question(
+            3,"What country does this flag belong to?",
+            R.drawable.ic_flag_of_belgium,
+            "Brazil",
+            "Belgium",
+            "Denmark",
+            "Kuwait",
+            2
+        )
+        questionsList.add(que3)
+        return questionsList
+    }
+
+}
